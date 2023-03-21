e03ae387181ab337f11b7b9e9b1f3318424b0203
Rana-Mudassir <mduasire38@gmail.com>
2023-03-21 16:18:03 +0500

    Added header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './index.css';
+import img from './images/logo.jpg';
+const image = document.getElementById('imageDiv');
+image.setAttribute("src", img);
+console.log("image", image)
+
+// document.getElementById("logo").append(img);
