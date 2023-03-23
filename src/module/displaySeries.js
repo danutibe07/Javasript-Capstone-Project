c23150187989c48c7dcb1f663007c2024dccf910
danutibe07 <danielutibe07@gmail.com>
2023-03-23 16:07:42 +0100

    using involve API to add like functionality to the user interface

---
+++ src/module/displaySeries.js
@@ -0,0 +1,28 @@
+import getSeries from "./getSeries";
+
+const displaySeries = (data) => {
+  const seriesapp = document.querySelector('.boxes');
+  const series = document.createElement('div');
+  series.classList.add('series');
+  series.innerHTML = `
+    <div> <img class="series-img" src="${data.image.medium}" alt=""></div>
+    <div class="details">
+        <h4 class="series-name">${data.name}</h4>
+        <div class="likebtn">
+        <span class="heart"></span>
+        <span class="countlike">34</span>
+        <span class="text">Likes</span>
+        </div>
+        <button id="comment">comment</button>
+    </div>`;
+  seriesapp.appendChild(series);
+};
+
+export default displaySeries;
+
+// const commentBtnfnx = () =>{
+//   const commentbtn = document.querySelectorAll("#comment")
+//   commentbtn.forEach((e) => {
+//       console.log("comment");
+//   });
+// }
