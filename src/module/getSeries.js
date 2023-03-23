9fb8dfef73fbd05a664896d68fec447b8557c50b
danutibe07 <danielutibe07@gmail.com>
2023-03-23 16:05:53 +0100

    updates to getSeries

--- src/module/getSeries.js
+++ src/module/getSeries.js
@@ -1,27 +1,27 @@
-import GetItems from './getItems.js';
-const getSeries = async () => {
-  const dataStream = await fetch('https://api.tvmaze.com/shows');
+ import { toggleLike } from "./LikeApi";
+ import { getLikes } from "./LikeApi";
+ const getSeries = async ()=>{
+ const dataStream = await fetch("https://api.tvmaze.com/shows");
   const dataResponse = await dataStream.json();
   dataResponse.forEach((element) => {
-    const seriesapp = document.querySelector('.boxes');
-    const series = document.createElement('div');
-    series.classList.add('series');
+    const seriesapp = document.querySelector(".boxes");
+    const series = document.createElement("div");
+    series.classList.add("series");
     series.innerHTML = `
       <div> <img class="series-img" src="${element.image.medium}" alt=""></div>
       <div class="details">
           <h4 class="series-name">${element.name}</h4>
-          <div class="likebtn">
-          <span class="heart"></span>
-          <span class="countlike">34</span>
-          <span class="text">Likes</span>
+          <div class="likebtn" >
+          <button class="btnlike material-symbols-outlined" data-id="${element.name}">favorite</button>
+          <span class="countlike"  id="count" data-id="${element.name}">0</span>
+          <span class="text"> Likes</span>
           </div>
           <button id="comment">Comment</button>
           <button id="reserve">Reservation</button>
       </div>`;
     seriesapp.appendChild(series);
   });
-  GetItems()
+  toggleLike();
+  getLikes()
 };
-
 export default getSeries;
-
